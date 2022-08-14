ec29ba417a0657c92390396246392e0cca9ad16a
MahinMuhammad <amimahinurrahman@gmail.com>
2022-08-14 23:09:50 +0600

    snow fall

--- ManhattanBridge/ManhattanBridge/main.cpp
+++ ManhattanBridge/ManhattanBridge/main.cpp
@@ -5,10 +5,20 @@
 #include <windows.h>
 #include <GL/glut.h>
 #endif
+#include<math.h>
+#include<cstdio>
+# define PI 3.14159265358979323846
+#include <cctype>
 
 void WinterView();
 void WinterSky();
 void WinterRiver();
+
+GLfloat snowPos = 0.0f;
+GLfloat snowSpd = 0.05f;
+void CircleSnow(GLfloat a, GLfloat b, GLdouble r,int R,int G,int B);
+void snowFall();
+void snowAnimation(int value);
 
 int main(int argc, char** argv)
 {
@@ -17,6 +27,8 @@ int main(int argc, char** argv)
     glutInitWindowSize(320, 320);
     
     glutDisplayFunc(WinterView);
+    glutPostRedisplay();
+    glutTimerFunc(100, snowAnimation, 0);
     
     glutMainLoop();
 
@@ -31,6 +43,11 @@ void WinterView()
     WinterRiver();
     
     WinterSky();
+    
+    glPushMatrix();
+    glTranslatef(0,snowPos,0);
+    snowFall();
+    glPopMatrix();
     
     glFlush();
 }
@@ -80,3 +97,82 @@ void WinterRiver()
 
     glLoadIdentity();
 }
+
+void CircleSnow(GLfloat a, GLfloat b, GLdouble r, int R, int G, int B)
+{
+    int i;
+    GLfloat x=a;
+    GLfloat y=b;
+    GLdouble radius =r;
+    int triangleAmount = 60;
+
+    GLfloat twicePi = 2.0f * PI;
+
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3ub(R, G, B);
+    glVertex2f(x, y);
+    for(i = 0; i <= triangleAmount; i++)
+    {
+        glVertex2f(
+            x + (radius * sin(i *  twicePi / triangleAmount)),
+            y + 2*(radius * cos(i * twicePi / triangleAmount))
+        );
+    }
+    glEnd();
+}
+
+void snowAnimation(int value)
+{
+
+    if(snowPos <-5)
+        snowPos = -4;
+
+    snowPos -= snowSpd;
+
+    glutPostRedisplay();
+
+
+    glutTimerFunc(100, snowAnimation, 0);
+}
+
+
+
+void snowFall()
+{
+    //CircleSnow(0,-0.5,0.01,190, 217, 244);'
+
+    int cnt = 0;
+    float posY = 0.99;
+
+    for(int i=0; i<100; i++)
+    {
+        float posX = -1.5;
+
+        for(int j=0; j<15; j++)
+        {
+            if(cnt%4==0)
+            {
+                posX +=0.33;
+            }
+            else if(cnt%3==0)
+            {
+                posX +=0.25;
+            }
+            else
+            {
+                posX +=0.15;
+            }
+            CircleSnow(posX,posY,0.008,240, 240, 240);
+            CircleSnow(posX,posY,0.005,198, 222, 245);
+
+
+            cnt++;
+
+        }
+        posY +=0.2;
+
+    }
+
+}
+
+
